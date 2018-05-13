4eddf82d8c08d621d6351ac1729ab7a22a71f6f3
JoinFeng <mottoboy@126.com>
2018-05-13 16:59:50 +0800

    Make a base android project.

---
+++ app/src/main/java/com/mark/makebase/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mark.makebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Observable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ common/src/main/java/com/mark/common/BaseApp.java
@@ -0,0 +1,14 @@
+package com.mark.common;
+
+import android.app.Application;
+
+import io.reactivex.Observable;
+
+public class BaseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
