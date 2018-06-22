ed51509a2dfca4efa37ebde95acfaf52282227a3
Joinfeng <mottoboy@126.com>
2018-06-22 10:22:43 +0800

    添加view放大缩小的工具类

---
+++ common/src/main/java/com/mark/common/util/ViewUtil.java
@@ -0,0 +1,176 @@
+package com.mark.common.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.TouchDelegate;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Adapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+import com.mark.common.R;
+
+
+/**
+ * Created by yinhaide on 2018/1/8.
+ *  marks:
+ *      <li>试用anim资源：zoom_in & zoom_out</li>
+ */
+
+public class ViewUtil {
+    private static ViewUtil instance=null;
+
+    public static ViewUtil shareInstance(){
+        if(instance==null){
+            instance=new ViewUtil();
+        }
+        return instance;
+    }
+
+    private ViewUtil(){}
+
+    public void HideKeyBoardWhenTouch(final Activity activity, int rootViewId){
+        activity.findViewById(rootViewId).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+        });
+    }
+
+    public void requestTouch(ListView listView, final ScrollView scrollView){
+        listView.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    scrollView.requestDisallowInterceptTouchEvent(false);
+                }else{
+                    scrollView.requestDisallowInterceptTouchEvent(true);
+                }
+                return false;
+            }
+        });
+    }
+
+    public void reSetListViewHeight(ListView listView) {
+        //获得adapter
+        Adapter adapter =listView.getAdapter();
+        if (adapter == null) {
+            return;
+        }
+        int totalHeight = 0;
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View listItem = adapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            //计算总高度
+            totalHeight += listItem.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        //计算分割线高度
+        params.height = totalHeight + (listView.getDividerHeight() * (adapter.getCount() - 1));
+        //给listview设置高度
+        listView.setLayoutParams(params);
+    }
+
+    public void setListViewHeightBasedOnChildren(ListView listView) {
+        // 获取ListView对应的Adapter
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) { // listAdapter.getCount()返回数据项的数目
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0); // 计算子项View 的宽高
+            totalHeight += listItem.getMeasuredHeight(); // 统计所有子项的总高度
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        // listView.getDividerHeight()获取子项间分隔符占用的高度
+        // params.height最后得到整个ListView完整显示需要的高度
+        listView.setLayoutParams(params);
+    }
+
+    /**
+     * View的放大缩小
+     * @param context
+     * @param mView
+     * @param isUp
+     */
+    public void setTouchChange(final Context context, View mView, final boolean isUp){
+        mView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN:
+                        beginScale(view,context, R.anim.zoom_in);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        beginScale(view,context,R.anim.zoom_out);
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        beginScale(view,context,R.anim.zoom_out);
+                        break;
+                }
+                return isUp;
+            }
+        });
+    }
+
+
+    /**
+     * 扩大View的触摸和点击响应范围,最大不超过其父View范围
+     *
+     * @param view
+     * @param top
+     * @param bottom
+     * @param left
+     * @param right
+     */
+    public void expandViewTouchDelegate(final View view, final int top, final int bottom, final int left, final int right) {
+        ((View) view.getParent()).post(new Runnable() {
+            @Override
+            public void run() {
+                Rect bounds = new Rect();
+                view.setEnabled(true);
+                view.getHitRect(bounds);
+                bounds.top -= top;
+                bounds.bottom += bottom;
+                bounds.left -= left;
+                bounds.right += right;
+                TouchDelegate touchDelegate = new TouchDelegate(bounds, view);
+                if (View.class.isInstance(view.getParent())) {
+                    ((View) view.getParent()).setTouchDelegate(touchDelegate);
+                }
+            }
+        });
+    }
+
+    private synchronized void beginScale(View view, Context context, int animation) {
+        Animation an = AnimationUtils.loadAnimation(context, animation);
+        an.setDuration(80);
+        an.setFillAfter(true);
+        view.startAnimation(an);
+    }
+
+    private static long lastClickTime;
+    public static boolean isFastDoubleClick() {
+        long time = System.currentTimeMillis();
+        long timeD = time - lastClickTime;
+        if ( 0 < timeD && timeD < 2000) {       //500毫秒内按钮无效，这样可以控制快速点击，自己调整频率
+            return true;
+        }
+        lastClickTime = time;
+        return false;
+    }
+}
